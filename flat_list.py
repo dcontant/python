1116c299995bec27de8fd5b7c411b0e4bb8444af
denys <denyscontant@gmail.com>
2018-11-28 10:41:11 -0500

    Create flat_list.py

---
+++ flat_list.py
@@ -0,0 +1,27 @@
+flat_list = lambda root_list: [token for sub_list in root_list for token in flat_list(sub_list)] \
+                               if type(root_list) is list else [root_list]
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
+    
+ 
+def flat_list(root_list):
+    if type(root_list) is list:
+        return [token for sub_list in root_list for token in flat_list(sub_list)]
+    else:
+        return [root_list]
+    
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
+    
+ '''
+ itertools.chain.from_iterable(root_list) will fail if an element of root_list is not an iterable ex: an integer
+ '''
