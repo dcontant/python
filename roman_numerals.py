b82d157c74c09d838b0de0639675f8476aea5432
dcontant <denyscontant@gmail.com>
2024-09-13 16:26:57 -0400

    Update roman_numerals.py

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,13 +1,13 @@
 def romans_numerals(n):
-    ''' from  Checkio  '''
     result = ''
     for arabic, roman in zip((1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
                              'M     CM   D    CD   C    XC  L   XL  X   IX V  IV I'.split()):
-        result += n // arabic * roman
+        result += roman * (n // arabic)
         n %= arabic
     return result
 
 if __name__ == '__main__':
+    assert romans_numerals(1) == 'I', '1'
     assert romans_numerals(6) == 'VI', '6'
     assert romans_numerals(76) == 'LXXVI', '76'
     assert romans_numerals(499) == 'CDXCIX', '499'
@@ -16,17 +16,21 @@ if __name__ == '__main__':
     
     
 def reverse_roman(roman_string):
-    romans = {'':0,'I':1,'IV':4,'V':5,'IX':9,'X':10,'XL':40,'L':50,'XC':90,'C':100,'CD':400,'D':500,'CM':900,'M':1000}
+    size = len(roman_string)
+    roman_string +=  ' ' * (size%2)  # add a blank space if size is odd
+    romans = {' ':0, 'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     ans = 0
-    for i,digit in enumerate(roman_string):
-        x,y = romans[digit],romans[roman_string[i+1:i+2]]
+    for i in range(0, size, 2):
+        x, y = roman_string[i: i+2]
+        x, y = romans[x], romans[y]
         if x < y:
-            ans += romans[roman_string[i:i+2]]-romans[roman_string[i+1]]
+            ans += (y - x)
         else:
-            ans += x
+            ans += (x + y)
     return ans
 
 if __name__ == '__main__':
+    assert reverse_roman('I') == 1, '1'
     assert reverse_roman('VI') == 6, '6'
     assert reverse_roman('LXXVI') == 76, '76'
     assert reverse_roman('CDXCIX') == 499, '499'
